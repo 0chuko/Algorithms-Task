d03834d45a6039674335a9610870820ad6f07334
Ochuko Larry Ighofua <80969919+0chuko@users.noreply.github.com>
2021-05-21 12:19:20 +0100

    Add files via upload

---
+++ convertFahrToCelsius.js
@@ -0,0 +1,19 @@
+const convertFahrToCelsius = (deg) => {
+    let parsedDeg;
+  
+    if (!Array.isArray(deg)) {
+      parsedDeg = parseInt(deg);
+    }
+    if (isNaN(parsedDeg)) {
+      return `${JSON.stringify(deg)} is not a valid number but a/an ${
+        Array.isArray(deg) ? "array" : typeof deg
+      }`;
+    }
+  
+    let celsius = ((deg - 32) * 5) / 9;
+    let c = celsius.toFixed(4);
+    console.log(c);
+    return `${c}`;
+  };
+  
+  console.log(convertFahrToCelsius(0));
